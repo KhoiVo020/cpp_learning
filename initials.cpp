c68af605c1c6ab4217f7526eec477df4e2b3cd43
KhoiNVo <107451712+KhoiNVo@users.noreply.github.com>
2023-04-21 14:50:08 -0700

    Create initials.cpp

---
+++ initials.cpp
@@ -0,0 +1,16 @@
+// Khoi Vo
+// Fun Fact: I played guitar in a band called Attica
+
+#include <iostream>
+
+int main(){
+std::cout << " K   K V    V \n";
+std::cout << " K  K  V    V \n";
+std::cout << " K K   V    V \n";
+std::cout << " KK    V    V \n";
+std::cout << " K K   V    V \n";
+std::cout << " K  K    V V  \n";
+std::cout << " K   K    V   \n";
+
+return 0;
+}
